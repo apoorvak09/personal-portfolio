ec4b7c088e277c6559e13be9436b5e7e2a203143
d-jaat <65522780+d-jaat@users.noreply.github.com>
2024-05-25 17:26:59 +0530

    Create header.php

---
+++ header.php
@@ -0,0 +1,11 @@
+<header>
+    <nav>
+        <ul>
+            <li><a href="index.php">Home</a></li>
+            <li><a href="about.php">About Me</a></li>
+            <li><a href="projects.php">Projects</a></li>
+            <li><a href="skills.php">Skills</a></li>
+            <li><a href="contact.php">Contact</a></li>
+        </ul>
+    </nav>
+</header>
